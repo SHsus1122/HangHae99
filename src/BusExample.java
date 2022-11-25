51c1e27c54e6eb3abea15ca5158ff8a68f7c1367
SHsus1122 <tjgurtn1122@naver.com>
2022-11-25 18:30:59 +0900

    과제 실패

---
+++ src/BusExample.java
@@ -0,0 +1,23 @@
+public class BusExample {
+    public static void main(String[] args) {
+        Bus myBus = new Bus();
+
+
+        System.out.println(myBus.getBusNum());
+        myBus.State();
+        for (int i = 0; i < 12; i++) {
+            myBus.ride();
+        }
+        System.out.println(myBus.getPrice());
+        myBus.setSpeed(100);
+        myBus.speedInfo();
+        myBus.setRideGuest(5);
+        myBus.guestCount();
+        myBus.setRideGuest(10);
+        myBus.guestCount();
+        myBus.setRideGuest(20);
+        myBus.guestCount();
+        System.out.println(myBus.getNowGuest());
+        myBus.State();
+    }
+}
---
+++ src/Taxi.java
@@ -0,0 +1,10 @@
+public class Taxi {
+    // 필드
+    static final int maxGuest = 4;
+    int nowGuest;
+    int rideGuest;
+    int price = 1800;
+    static final int busNum = 5593;
+    int oil = 100;
+    int speed;
+}
